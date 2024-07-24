c67ed843d5604ed058fc3908bb3184c728197281
Umuhuza Clever <umuhuzaclever@Umuhuzas-Laptop.local>
2024-07-24 22:45:49 +0200

    add es6 classes

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+import Currency from "./3-currency";
+/* eslint-enable no-unused-vars */
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
